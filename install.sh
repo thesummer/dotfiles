70d2106715fd607d7a512ac694c166dff2d8f9cc
Jan Sommer <jan.sommer@dlr.de>
2020-01-29 11:21:38 +0100

    Remove applescripts

--- install.sh
+++ install.sh
@@ -8,49 +8,12 @@ echo "Installing dotfiles."
 
 source install/link.sh
 
-source install/git.sh
+# source install/git.sh
 
-# only perform macOS-specific install
-if [ "$(uname)" == "Darwin" ]; then
-    echo -e "\\n\\nRunning on macOS"
+# echo "creating vim directories"
+ mkdir -p ~/.vim-tmp
 
-    if test ! "$( command -v brew )"; then
-        echo "Installing homebrew"
-        ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
-    fi
-
-    # install brew dependencies from Brewfile
-    brew bundle
-
-    # After the install, setup fzf
-    echo -e "\\n\\nRunning fzf install script..."
-    echo "=============================="
-    /usr/local/opt/fzf/install --all --no-bash --no-fish
-
-    # after the install, install neovim python libraries
-    echo -e "\\n\\nRunning Neovim Python install"
-    echo "=============================="
-    pip3 install pynvim
-
-    # Change the default shell to zsh
-    zsh_path="$( command -v zsh )"
-    if ! grep "$zsh_path" /etc/shells; then
-        echo "adding $zsh_path to /etc/shells"
-        echo "$zsh_path" | sudo tee -a /etc/shells
-    fi
-
-    if [[ "$SHELL" != "$zsh_path" ]]; then
-        chsh -s "$zsh_path"
-        echo "default shell changed to $zsh_path"
-    fi
-
-    source install/osx.sh
-fi
-
-echo "creating vim directories"
-mkdir -p ~/.vim-tmp
-
-if ! command_exists zsh; then
+ if ! command_exists zsh; then
     echo "zsh not found. Please install and then re-run installation scripts"
     exit 1
 elif ! [[ $SHELL =~ .*zsh.* ]]; then
