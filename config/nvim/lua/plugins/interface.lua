6670b10b2477c87111aa6bc7c4528e8edf416246
Jan Sommer <jan.sommer@dlr.de>
2026-04-09 11:34:28 +0200

    vim: Improve copy to clipboard
    Ensure that yanked data is also available on Windows (via Windows
    Terminal)

--- config/nvim/lua/plugins/interface.lua
+++ config/nvim/lua/plugins/interface.lua
@@ -81,4 +81,22 @@ return {
       end,
     },
   },
+  {
+    "ojroques/nvim-osc52",
+    config = function()
+      require("osc52").setup()
+      vim.keymap.set("n", "<leader>c", require("osc52").copy_operator, { expr = true })
+      vim.keymap.set("n", "<leader>cc", "<leader>c_", { remap = true })
+      vim.keymap.set("v", "<leader>c", require("osc52").copy_visual)
+
+      -- Auto-copy to clipboard on yank
+      vim.api.nvim_create_autocmd("TextYankPost", {
+        callback = function()
+          if vim.v.event.operator == "y" then
+            require("osc52").copy(vim.fn.getreg('"'))
+          end
+        end,
+      })
+    end,
+  },
 }
